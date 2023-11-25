64c2bf2b5b693e076ca9c29c3f7bcafd793e40e4
Fernando <fsetiend@gmail.com>
2023-11-25 20:32:50 +0100

    timestamp

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(express.errorHandler());
 // Add any new real-time connection to the `everybody` channel
 app.on("connection", (connection) => app.channel("everybody").join(connection));
 // Publish all events to the `everybody` channel
-app.publish((data) => app.channel("everybody"));
+app.publish(() => app.channel("everybody"));
 //
 
 //express route
